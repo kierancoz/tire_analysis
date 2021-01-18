4345b6d3bd931bbf2ef382abdfc9d234cba41868
kierancoz <kierancoz.w@gmail.com>
2021-01-17 18:01:06 -0600

    Data organized

---
+++ data_helpers.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def clean_data(datas):
+    return_df = None
+    for data in datas:
+        required_length = len(data["FY"])
+        for x in list(data.keys()):
+            if len(data[x]) != required_length:
+                del data[x] # removes unnecessary data
+            else:
+                data[x] = data[x].transpose()[0] # cleans necessary data
+        return_df = pd.concat([return_df, pd.DataFrame.from_dict(data)])
+    return return_df
+
+def get_nearest_value(possible_values, input_value):
+    closest_value, distance = None, 0
+    for value in possible_values:
+        test_dist = abs(value - input_value)
+        if not distance or test_dist < distance:
+            distance = test_dist
+            closest_value = value
+    return closest_value
--- plot_data.py
+++ plot_data.py
@@ -1,25 +1,36 @@
 import scipy.io as sio
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 import numpy as np
+import data_helpers
 
-data = sio.loadmat("rear_tire/RunData_10inch_Cornering_Matlab_SI_Round6/B1654run21.mat")
-data2 = sio.loadmat("rear_tire/RunData_10inch_Cornering_Matlab_SI_Round6/B1654run22.mat")
+# take data and turn it into a pandas dataframe
+data_files_names = ["rear_tire/RunData_10inch_Cornering_Matlab_SI_Round6/B1654run21.mat",
+                "rear_tire/RunData_10inch_Cornering_Matlab_SI_Round6/B1654run22.mat"]     
+   
+datas = [sio.loadmat(file_name) for file_name in data_files_names]
 
-def clean_data(data):
-    required_length = len(data["FY"])
-    for x in list(data.keys()):
-        if len(data[x]) != required_length:
-            del data[x] # removes unnecessary data
-        else:
-            data[x] = data[x].transpose()[0] # cleans necessary data
+df = data_helpers.clean_data(datas)
 
-clean_data(data)
-clean_data(data2)
-df = pd.concat([pd.DataFrame.from_dict(data), pd.DataFrame.from_dict(data2)])
+# create clasification columns based on sweeps
+variable_sweeps = {"load" : {"sweep" : np.array([-250, -200, -150, -100, -50]) / 0.224809, "label" : "FZ" },
+                    "camber" : {"sweep" : np.array([0, 2, 4]), "label" : "IA"},
+                    "pressure" : {"sweep" : np.array([8, 10, 12, 14]) * 6.89476, "label" : "P"},
+                    "velocity" : {"sweep" : np.array([15, 25, 45]) * 1.60934, "label" : "V"}}
 
-print(df.shape)
-print(df.head)
+for variable, info in variable_sweeps.items():
+    temp_nearest_func = lambda x: data_helpers.get_nearest_value(info["sweep"], x)
+    df[variable] = df[info["label"]].apply(temp_nearest_func)
 
-plt.plot(df["SA"], df["FY"])
+# plot each classification as a different color on a slip angle vs lateral force graph
+# for i, g in df.groupby(list(variable_sweeps.keys())):
+#     plt.plot(g["SA"], g["FY"])
+# plt.show()
+
+# plot a specific set of conditions on a graph
+conditions = df[(df["camber"] == variable_sweeps["camber"]["sweep"][0]) & (df["velocity"] == variable_sweeps["velocity"]["sweep"][0])]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_wireframe(conditions["SA"], conditions["FZ"], np.array([conditions["FY"], conditions["FY"]]))
 plt.show()
