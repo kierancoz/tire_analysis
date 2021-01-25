7322ea22d695f4ef71a3817f6301bc386c4b2f7c
kierancoz <kierancoz.w@gmail.com>
2021-01-25 15:48:21 -0600

    Semi working state pacejka

--- plot_data.py
+++
@@ -1,9 +0,0 @@
-# OLD FILE
-
-
-# how to plot 3D graph
-# conditions = df[(df["camber"] == variable_sweeps["camber"]["sweep"][0]) & (df["velocity"] == variable_sweeps["velocity"]["sweep"][0])]
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.plot_wireframe(conditions["SA"], conditions["FZ"], np.array([conditions["FY"], conditions["FY"]]))
-# plt.show()
