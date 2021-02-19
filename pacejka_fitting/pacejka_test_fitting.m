64b521a3670070668d47c335aee1ca9690fdfae1
kierancoz <kierancoz.w@gmail.com>
2021-02-18 18:38:11 -0600

    test

--- pacejka_fitting/pacejka_test_fitting.m
+++ pacejka_fitting/pacejka_test_fitting.m
@@ -1,40 +1,43 @@
 clear
-load("processed_data.mat");
-hold on;
+load("processed_data2.mat");
 
-f = fittype('D*sin(C * atan(B * x - E * (B * (x + Sh) - atan(B * (x + Sh))))) + Sv',...
-    'dependent', {'y'}, 'independent', {'x'},...
-    'coefficients', {'D','B', 'E', 'C', 'Sh', 'Sv'});
+% B = (a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz)))
+f = fittype('(a1*fz^2 + a2*fz)*sin(C * atan((a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz))) * x - (a6*fz^2 + a7*fz + a8) * ((a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz))) * (x + Sh) - atan((a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz))) * (x + Sh))))) + Sv',...
+    'dependent', {'y'}, 'independent', {'x', 'fz'},...
+    'coefficients', {'C', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'Sh', 'Sv'});
 
 %f = fittype('lateralForcePajecka(x, D, B, E, Sh, Sv)');
 %f = fittype('test(x, D, B)');
 
 options = fitoptions(f);
 options.MaxIter = 10000;
-options.StartPoint = [2625, 0.15, 0.06, 9.4, -79.5];
+options.StartPoint = [1.3, -22, 1000, 1, 1, 1, 1, 1, 1, 9.4, -79.5];
 options.MaxFunEvals = 10000; 
 
-D_startvals = [2622, 2152, 1745, 1200, 1000];
+%D_startvals = [2622, 2152, 1745, 1200, 1000];
 standard_devs = 0;
-for i = 0:4
-    load_name = "load" + i;
-    options.StartPoint(1) = D_startvals(i + 1);
-    for j = 0:2
-        camber_name = "camber" + j;
-        data = eval(load_name + "." + camber_name);
+for i = 0:1
+    camber_name = "camber" + i;
+    for j = 0:1
+        %load_name = "load" + i;
+        %options.StartPoint(1) = D_startvals(i + 1);
         
-        [fit1,gof,fitinfo] = fit(data.SA.',data.FY.',f,options);
+        data = eval(camber_name);
+        
+        [fit1,gof,fitinfo] = fit([data.SA.' data.FZ.'],data.FY.',f,options);
         x = fitinfo.residuals.^2;
         standard_dev = (sum(x)/size(x,1))^0.5;
         standard_devs = standard_devs + standard_dev;
         
-        eval(load_name + "." + camber_name + ".sd = " + standard_dev + ";");
+        %eval(load_name + "." + camber_name + ".sd = " + standard_dev + ";");
         %eval(load_name + "." + camber_name + ".fit = " + fitinfo);
-        %plot(fit1);
-        %plot(data.SA.',data.FY.');
+        plot3(data.SA.', data.FZ.',data.FY.');
+        hold on
+        plot(fit1);
         break
         %fit1.C
     end
+    break
 end
 standard_devs
 %plot(load4.camber0.SA.',load4.camber0.FY.','r')
