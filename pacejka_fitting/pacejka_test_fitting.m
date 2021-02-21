8f640340dd693749d03f6515888c10b48621c203
kierancoz <kierancoz.w@gmail.com>
2021-02-21 11:30:27 -0600

    Temp

--- pacejka_fitting/pacejka_test_fitting.m
+++ pacejka_fitting/pacejka_test_fitting.m
@@ -2,9 +2,9 @@ clc
 clear
 load("processed_data2.mat");
 
-f = fittype('(a1*fz^2 + a2*fz)*sin(C * atan(B * x - E * (B * (x + Sh) - atan(B * (x + Sh))))) + Sv',...
+f = fittype('(a1*fz^2 + a2*fz)*sin(a0 * atan(B * x - E * (B * (x + 0) - atan(B * (x + 0))))) + 0',...
     'dependent', {'y'}, 'independent', {'x', 'fz'},...
-    'coefficients', {'a1', 'a2','B', 'E', 'C', 'Sh', 'Sv'});
+    'coefficients', {'a1', 'a2', 'a0', 'B', 'E'});
 
 % B = (a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz)))
 %f = fittype('(a1*fz^2 + a2*fz)*sin(C * atan((a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz))) * x - (a6*fz^2 + a7*fz + a8) * ((a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz))) * (x + Sh) - atan((a3 * sin( a4 * atan(a5 * fz)) / (C * (a1 * fz^2 + a2 * fz))) * (x + Sh))))) + Sv',...
@@ -13,7 +13,7 @@ f = fittype('(a1*fz^2 + a2*fz)*sin(C * atan(B * x - E * (B * (x + Sh) - atan(B *
 
 options = fitoptions(f);
 options.MaxIter = 10000;
-options.StartPoint = [-0.0003, -2.6480, 0.15, 0.06, -1.5, 9.4, -79.5];
+options.StartPoint = [-0.0006034, -3.031, -1.608, 0.1484, 0.3817];
 options.MaxFunEvals = 10000; 
 
 for i = 0:2
@@ -21,14 +21,15 @@ for i = 0:2
     data = eval(camber_name);
 
     [fit1,gof,fitinfo] = fit([data.SA.' data.FZ.'],data.FY.',f,options);
-    standard_dev = gof.rmse;
+    standard_dev = gof.rmse
     
     % plot 
     %eval(camber_name + ".fit = " + fitinfo);
     plot3(data.SA.', data.FZ.', data.FY.');
     hold on;
-    %plot(fit1);
-    fit1.Sh
+    plot(fit1);
+    %fit1.Sh
+    break
 end
 
 test_sa = -13+0.4*(0:65);
