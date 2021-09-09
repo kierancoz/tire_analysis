6ca571bc4b211f8a01c89eba70fffd08510fd97b
kierancoz <kierancoz.w@gmail.com>
2021-09-09 11:56:31 -0500

    semi-working state

--- pacejka_fitting/fitting_cornering_2021_rear.m
+++ pacejka_fitting/fitting_cornering_2021_rear.m
@@ -1,16 +1,79 @@
 clc
 clear
 % requires tire_data repo to be initialized. run 'make get_data' in
-% repo folder to get data. Request access if command fails
+% repo folder to get data. Request access to repo if command fails
 load(fileparts(matlab.desktop.editor.getActiveFilename) + "/../tire_data/processed_data/cornering_2021_rears.mat");
 
-lateral_pacejka.call_1(5)
+%lateral_pacejka.call_1(1)
 
-f = fittype('lateral_pacejka.call_1(x, fz, ia, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17)',...
+f = fittype('lateral_pacejka.call_1(x, fz, ia, a0, a1, a2, a3, a4, a5, a6)',...
+    'independent', {'x', 'fz'}, 'problem', 'ia',...
+    'coefficients', {'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6'});
+fo = fitoptions(f);
+fo.MaxIter = 10000;
+%fo.StartPoint = [1.4, 2.56, 0.159, 0.36, -22, -0.32, 0];
+fo.MaxFunEvals = 10000;
+
+
+% filter out unwanted pressure and velocities
+req_SA = [];
+req_FZ = [];
+req_FY = [];
+req_IA = [];
+
+unique_vels = [40.2335, 24.1401, 72.4203];
+unique_press = [82.73712, 68.9476, 96.5266, 55.1581];
+for i = 1:length(FZ)
+    if any(1==find(unique_vels==velocity(1,i))) && any(1==find(unique_press==pressure(1,i)))
+        req_SA(end+1) = SA(1, i);
+        req_FZ(end+1) = FZ(1, i);
+        req_FY(end+1) = FY(1, i);
+        req_IA(end+1) = IA(1, i);
+    end
+end
+
+%%% FIRST FIT
+%fit data
+[fit1,gof,fitinfo] = fit([req_SA.' req_FZ.'], req_FY.', f, fo, 'problem', req_IA.');
+disp("standard_dev is: " + gof.rmse);
+
+
+%%% FIT 2
+f = fittype('lateral_pacejka.call_2(x, fz, ia, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10)',...
+    'independent', {'x', 'fz'}, 'problem', 'ia',...
+    'coefficients', {'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10'});
+fo = fitoptions(f);
+fo.MaxIter = 10000;
+fo.StartPoint = [coeffvalues(fit1) 0 0 0 0];
+fo.MaxFunEvals = 10000;
+
+% fit data
+[fit1,gof,fitinfo] = fit([req_SA.' req_FZ.'], req_FY.', f, fo, 'problem', req_IA.');
+disp("standard_dev is: " + gof.rmse);
+
+
+%%% FIT 3
+f = fittype('lateral_pacejka.call_3(x, fz, ia, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12)',...
+    'independent', {'x', 'fz'}, 'problem', 'ia',...
+    'coefficients', {'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11', 'a12'});
+fo = fitoptions(f);
+fo.MaxIter = 10000;
+fo.StartPoint = [coeffvalues(fit1) 0 0];
+fo.MaxFunEvals = 10000;
+
+% fit data
+[fit1,gof,fitinfo] = fit([req_SA.' req_FZ.'], req_FY.', f, fo, 'problem', req_IA.');
+disp("standard_dev is: " + gof.rmse);
+
+%%% FIT 4
+f = fittype('lateral_pacejka.call_4(x, fz, ia, a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17)',...
     'independent', {'x', 'fz'}, 'problem', 'ia',...
     'coefficients', {'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11', 'a12', 'a13', 'a14', 'a15', 'a16', 'a17'});
 fo = fitoptions(f);
 fo.MaxIter = 10000;
-fo.StartPoint = [0.01171, -0.0316, 270.6,  -647.5, -1491, 0.03917,...
-    -0.0003, 0.936, -4.58e-05, -0.076, -0.1177, 0.02542, 3.375, 5.507e-05, 0.04993, 0.005224, 0.00139, 0.00443];
+fo.StartPoint = [coeffvalues(fit1) 0 0 0 0 0];
 fo.MaxFunEvals = 10000;
+
+% fit data
+[fit1,gof,fitinfo] = fit([req_SA.' req_FZ.'], req_FY.', f, fo, 'problem', req_IA.');
+disp("standard_dev is: " + gof.rmse);
--- pacejka_fitting/lateral_pacejka.m
+++ pacejka_fitting/lateral_pacejka.m
@@ -1,19 +1,51 @@
 classdef lateral_pacejka
     methods (Static)
-        function force = call_1(x, fz, ia, D, C, Bx1, E, V)
-            %C = a0;
-            D = fz.*D; %(a1.*fz+a2).*(1-a15*ia.^2);
-            %BCD = a3.*sin(atan(fz./a4).*2).*(1-a5.*abs(ia));
-            %B = BCD./(C.*D);
+        function force = call_1(x, fz, ia, a0, a2, a3, a4, a7, a12, a9) 
+            C = a0;
+            D = fz.*a2;
+            BCD = a3.*sin(atan(fz./a4).*2);
+            B = BCD./(C.*D);
+            E = a7;
+            H = a9;
+            V = a12;
+            Bx1 = B.*(x + H);
+            force = D.*sin(C.* atan(Bx1 - E.*(Bx1 - atan(Bx1)))) + V;
+        end
+        % add all aditional load dependent terms
+        function force = call_2(x, fz, ia, a0, a2, a3, a4, a7, a12, a9, a1, a6, a8, a11) 
+            C = a0;
+            D = fz.*(a1.*fz+a2);
+            BCD = a3.*sin(atan(fz./a4).*2);
+            B = BCD./(C.*D);
+            E = (a6.*fz + a7);
+            H = a8.*fz + a9;
+            V = a11.*fz + a12;
+            Bx1 = B.*(x + H);
+            force = D.*sin(C.* atan(Bx1 - E.*(Bx1 - atan(Bx1)))) + V;
+        end
+        % add additional slip angle term (finish Curvature factor E)
+        function force = call_3(x, fz, ia, a0, a2, a3, a4, a7, a12, a9, a1, a6, a8, a11, a16, a17) 
+            C = a0;
+            D = fz.*(a1.*fz+a2);
+            BCD = a3.*sin(atan(fz./a4).*2);
+            B = BCD./(C.*D);
+            H = a8.*fz + a9;
+            E = (a6.*fz + a7).*(1 - (a16.*ia + a17).*sign(x + H));
+            V = a11.*fz + a12;
+            Bx1 = B.*(x + H);
+            force = D.*sin(C.* atan(Bx1 - E.*(Bx1 - atan(Bx1)))) + V;
+        end
+        % add additional camber terms
+        function force = call_4(x, fz, ia, a0, a2, a3, a4, a7, a12, a9, a1, a6, a8, a11, a16, a17, a15, a5, a10, a13, a14) 
+            C = a0;
+            D = fz.*(a1.*fz+a2).*(1-a15*ia.^2);
+            BCD = a3.*sin(atan(fz./a4).*2).*(1-a5.*abs(ia));
+            B = BCD./(C.*D);
             H = a8.*fz + a9 + a10.*ia;
             E = (a6.*fz + a7).*(1 - (a16.*ia + a17).*sign(x + H));
             V = a11.*fz + a12 + (a13.*fz + a14).*ia.*fz;
-            Bx1 = B.*(x);% + H);
-            y = D.*sin(C.* atan(Bx1 - E.*(Bx1 - atan(Bx1)))) + V;
-        end
-        
-        function force = call_2()
-            force = 2;
+            Bx1 = B.*(x + H);
+            force = D.*sin(C.* atan(Bx1 - E.*(Bx1 - atan(Bx1)))) + V;
         end
     end
 end
